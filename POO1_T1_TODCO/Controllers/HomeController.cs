8f40751731b24614eeee30a60398e4537dd9d712
Administrador <Administrador@DESKTOP-2NV77L8>
2026-09-23 03:02:41 -0500

    Agregar archivos de proyecto.

---
+++ POO1_T1_TODCO/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using POO1_T1_TODCO.Models;
+using System.Diagnostics;
+
+namespace POO1_T1_TODCO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ POO1_T1_TODCO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POO1_T1_TODCO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
